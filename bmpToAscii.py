99daaee752b0c093cc332f2ad5e5cc007b6b247c
Dennis Kronbügel <hubidennis@web.de>
2019-03-14 02:01:17 +0100

    Monchrome Bitmaps werden nun auf die Konsole gezeichnet

--- bmpToAscii.py
+++ bmpToAscii.py
@@ -1,3 +1,5 @@
+import math
+
 def readBytes(fileName):
     file = open(fileName, "rb", 0)
     bytes = file.read(-1)
@@ -26,8 +28,9 @@ def getHeight(bytes):
     return int.from_bytes(bytes[0x16:0x1a], 'little')
 
 def getPixelArrayBytes(bytes):
-    return bytes[getPixelArrayOffset(bytes):]  #TODO, die Länge stimmt wohl noch nicht, irgendwie muss ich die Länge greifen/berechnen
+    return bytes[getPixelArrayOffset(bytes):]
 
+#Obsolete Funktion
 def sortPixelsByRow(pixels, rowLength):
     rows = []
     row = []
@@ -41,30 +44,48 @@ def sortPixelsByRow(pixels, rowLength):
     return rows
 
 def getPixels(bytes):
+    #Todo: Die Bits werden Zeilenweise geschrieben. Eine neue Zeile fängt mit einem neuen Byte an.
+    #      Wieviele Bytes eine Zeile hat, ergibt sich aus der width des Bildes!
+    #      Bytes kommen nur in 4er Sequenzen vor (4Bytes, 8Bytes, 12 Bytes ...)
+    #      Bsp.: Width 10 => 4 Bytes | (RoundUp(10 / 8)) = 2  -> 4er Byte Blocks: RoundUp(2/4) * 4 = 4
+    #            Width 30 => 4 Bytes | (RoundUp(30 / 8)  = 4  -> 4er Byte Blocks: RoundUp(4/4) * 4 = 4
+    #            Width 33 => 8 Bytes | (RoundUp(33 / 8)  = 5  -> 4er Byte Blocks: RoundUp(5/4) * 4 = 8
     pixels = []
-    for byte in getPixelArrayBytes(bytes):
-        for bit in bin(byte)[2:]:
-            pixels.append(bit)
-    # Ok now we have ['1', '1', '1', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0']
-    return sortPixelsByRow(pixels, getWidth(bytes))
+    requiredBytesPerRow = math.ceil(getWidth(bytes) / 8)
+    bytesPerRow = math.ceil(requiredBytesPerRow / 4) * 4
+    rowCount = getHeight(bytes)
+    pixelArrayBytes = getPixelArrayBytes(bytes)
+    for i in range(0, rowCount):
+        sliceStart = (i * bytesPerRow)
+        sliceStop = sliceStart + bytesPerRow
+        rowBytes = pixelArrayBytes[sliceStart:sliceStop]
+        pixelRow = []
+        for rowByte in rowBytes:
+            for bit in bin(rowByte)[2:].rjust(8,"0"):
+                pixelRow.append(bit)
+        pixels.append(pixelRow[:getWidth(bytes)])
+    pixels = reversed(pixels)
+    return pixels
 
-def paint():
-    pass
+def paint(pixels):
+    for row in pixels:
+        for pixel in row:
+            if(pixel == "1"):
+                print("\u25a1", end="") #White square
+            else:
+                print("\u2b1b", end="") #Black square
+        print()
 
 def printMetaData(bytes):
-    print("Width:  " + str(getWidth(bytes)))
-    print("Height: " + str(getHeight(bytes)))
+    print("Size: " + str(getWidth(bytes)) + " x " + str(getHeight(bytes)))
+
 
 #############################
 # Format: BMP (Monochrome)  #
 #############################
-bytes = readBytes("monochrom10x2.bmp")
+#TODO Check for monochrome Bitmap (see Headers)
+bytes = readBytes("monochromHelloWorld.bmp")
 printMetaData(bytes)
 pixels = getPixels(bytes)
 paint(pixels)
-
-
-
-#print(bin(pixels[0]))
-
 

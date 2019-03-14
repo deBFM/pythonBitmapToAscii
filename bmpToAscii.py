1bc8d63f5127b33a16ff4472acc11d7fea95829b
Dennis Kronbügel <hubidennis@web.de>
2019-03-14 01:28:45 +0100

    Bits werden nun in einer entsprechenden Struktur aufgebaut

--- bmpToAscii.py
+++ bmpToAscii.py
@@ -1,5 +1,5 @@
-def readBytes():
-    file = open("mono.bmp", "rb", 0)
+def readBytes(fileName):
+    file = open(fileName, "rb", 0)
     bytes = file.read(-1)
     file.close()
     return bytes
@@ -25,8 +25,31 @@ def getHeight(bytes):
     """
     return int.from_bytes(bytes[0x16:0x1a], 'little')
 
-def getPixelArray(bytes):
+def getPixelArrayBytes(bytes):
     return bytes[getPixelArrayOffset(bytes):]  #TODO, die Länge stimmt wohl noch nicht, irgendwie muss ich die Länge greifen/berechnen
+
+def sortPixelsByRow(pixels, rowLength):
+    rows = []
+    row = []
+    count = 1
+    for pixel in pixels:
+        row.append(pixel)
+        if count % rowLength == 0:
+            rows.append(row)
+            row = []
+        count += 1
+    return rows
+
+def getPixels(bytes):
+    pixels = []
+    for byte in getPixelArrayBytes(bytes):
+        for bit in bin(byte)[2:]:
+            pixels.append(bit)
+    # Ok now we have ['1', '1', '1', '0', '0', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0']
+    return sortPixelsByRow(pixels, getWidth(bytes))
+
+def paint():
+    pass
 
 def printMetaData(bytes):
     print("Width:  " + str(getWidth(bytes)))
@@ -35,9 +58,11 @@ def printMetaData(bytes):
 #############################
 # Format: BMP (Monochrome)  #
 #############################
-bytes = readBytes()
+bytes = readBytes("monochrom10x2.bmp")
 printMetaData(bytes)
-pixels = getPixelArray(bytes)
+pixels = getPixels(bytes)
+paint(pixels)
+
 
 
 #print(bin(pixels[0]))
